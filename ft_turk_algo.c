a126612e4e96d7b1c07873be3a7a77368b5060bb
User <user@mbp-de-user.home>
2023-11-13 21:55:02 +0100

    wip

--- ft_turk_algo.c
+++ ft_turk_algo.c
@@ -56,7 +56,7 @@ void	ft_find_cheapest_cost(t_list *node_a, t_list *node_b)
 	while (node_b)
 	{
 		target = ft_find_target_node(node_a, node_b);
-//		printf("\ntarget_node de %d est %d\n", node_b->content, target->content);
+		//		printf("\ntarget_node de %d est %d\n", node_b->content, target->content);
 		node_b->b_price = node_b->index;
 		if (node_b->index > median_b)
 		{
@@ -177,7 +177,7 @@ void	ft_birdnest(t_list **node_a, t_list **node_b)
 		target = ft_find_target_node(*node_a, *node_b);
 		ft_find_cheapest_cost(*node_a, *node_b);
 		tmp = ft_find_min_cost(*node_b);
-		printf("bonjour antoine : b|%d| a|%d| t|%d|\n", tmp->b_price, tmp->a_price, tmp->total_price);
+		printf("bonjour antoine : b|%d| a|%d| target de b|%d|\n", tmp->b_price, tmp->a_price, (*node_b)->target);
 		ft_birdnest2(node_a, node_b, &tmp);
 	}
 }
@@ -185,51 +185,51 @@ void	ft_birdnest(t_list **node_a, t_list **node_b)
 void	ft_birdnest2(t_list **a, t_list **b, t_list **tmp)
 {
 	while ((*tmp)->b_price > 0 && (*tmp)->a_price > 0)
-		{
-			ft_rotate_both(a, b);
-			(*tmp)->b_price -= 1;
-			(*tmp)->a_price -= 1;
-		}
-		printf("a\n");
-		while ((*tmp)->b_price < 0 && (*tmp)->a_price < 0)
+	{
+		ft_rotate_both(a, b);
+		(*tmp)->b_price -= 1;
+		(*tmp)->a_price -= 1;
+	}
+	printf("a\n");
+	while ((*tmp)->b_price < 0 && (*tmp)->a_price < 0)
 		///while ((*tmp)->b_price < 0 && (*tmp)->a_price < 0)
-		{
-			ft_reverse_rotate_both(a, b);	
-			(*tmp)->b_price += 1;
-			(*tmp)->a_price += 1;
-		}
-		printf("b\n");
-		while ((*tmp)->b_price > 0)
-	//	while ((*tmp)->b_price > 0)
-		{
-			ft_rotate(b);
-			(*tmp)->b_price -= 1;
-		}
-		printf("c\n");
-		while ((*tmp)->a_price > 0)
-//		while ((*tmp)->a_price > 0)
-		{
-			ft_rotate(a);	
-			(*tmp)->a_price -= 1;
-		}
-		printf("d\n");
-		while ((*tmp)->b_price < 0) 
-//		while ((*tmp)->b_price < 0)
-		{
-			ft_reverse_rotate(b);	
-			(*tmp)->b_price += 1;
-		}
-		printf("e\n");
-		while ((*tmp)->b_price < 0)
+	{
+		ft_reverse_rotate_both(a, b);	
+		(*tmp)->b_price += 1;
+		(*tmp)->a_price += 1;
+	}
+	printf("b\n");
+	while ((*tmp)->b_price > 0)
+		//	while ((*tmp)->b_price > 0)
+	{
+		ft_rotate(b);
+		(*tmp)->b_price -= 1;
+	}
+	printf("c\n");
+	while ((*tmp)->a_price > 0)
+		//		while ((*tmp)->a_price > 0)
+	{
+		ft_rotate(a);	
+		(*tmp)->a_price -= 1;
+	}
+	printf("d\n");
+	while ((*tmp)->b_price < 0) 
+		//		while ((*tmp)->b_price < 0)
+	{
+		ft_reverse_rotate(b);	
+		(*tmp)->b_price += 1;
+	}
+	printf("e\n");
+	while ((*tmp)->b_price < 0)
 		///while ((*tmp)->a_price < 0)
-		{
-			printf("coucou petite perruche\n");
-			ft_reverse_rotate(a);
-			printf("c'est un elan qui a 2/3 ans\n");
-			(*tmp)->a_price += 1;
-		}
-		printf("COUCOU\n");
-		ft_push(b, a);
-		(printf("-6-\nA :\n"), ft_print_list(*a), ((*b) ? printf("\nB :\n"), ft_print_list(*b) : printf("b est vide\n\n")), printf("\n"));
-//	}
-}
+	{
+		printf("coucou petite perruche\n");
+		ft_reverse_rotate(a);
+		printf("c'est un elan qui a 2/3 ans\n");
+		(*tmp)->a_price += 1;
+	}
+	printf("COUCOU\n");
+	ft_push(b, a);
+	(printf("-6-\nA :\n"), ft_print_list(*a), ((*b) ? printf("\nB :\n"), ft_print_list(*b) : printf("b est vide\n\n")), printf("\n"));
+	//	}
+	}
