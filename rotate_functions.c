5c21f0ce055eb76ea0554b8c77da02543f56eb4f
elsa2491 <elsa.touma@gmail.com>
2023-11-17 18:39:28 +0100

    marche pas

--- rotate_functions.c
+++ rotate_functions.c
@@ -6,7 +6,7 @@
 /*   By: eltouma <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 16:18:01 by eltouma           #+#    #+#             */
-/*   Updated: 2023/11/15 17:22:27 by eltouma          ###   ########.fr       */
+/*   Updated: 2023/11/17 18:36:12 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,29 @@
 
 void	ft_swap_first_and_last(t_list **list)
 {
-	t_list	*last_node;
-//	int		tmp;
+	t_list	*avant_dernier;
+	t_list	*deuxieme;
 	t_list	*tmp;
 
-	last_node = ft_lstlast(*list);
+	tmp = ft_lstlast(*list);
+	avant_dernier = tmp->previous;
+	deuxieme = (*list)->next;
+
+	(*list)->next = NULL;
+	tmp->previous = NULL;
+
+	(*list)->previous = avant_dernier;
+	tmp->next = deuxieme;
+
+	avant_dernier->next = *list;
+	deuxieme->previous = tmp;
+
+	tmp = avant_dernier->next;
+	(*list) = deuxieme->next;
+	
 //	tmp = (*list)->content;
 //	(*list)->content = last_node->content;
 //	last_node->content = tmp;
-	tmp = *list;
-	*list = last_node;
-	last_node = tmp;
-	(*list)->previous = NULL;
-	(*list)->next->previous = *list;
-	(*list)->next = tmp->next;
-	last_node->previous = tmp->previous;
-	last_node->next = NULL;	
-	last_node->previous->next = last_node;
 }
 
 void	ft_reverse_rotate(t_list **list)
